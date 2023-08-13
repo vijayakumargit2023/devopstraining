d1506017edf265479d1d6b7c6ae9e3289333459e
vijayakumargit2023 <140235068+vijayakumargit2023@users.noreply.github.com>
2023-08-13 12:50:18 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ import java.io.*;
 class HelloWorld {
       public static void main(String args[]) {
           System.out.println("Hello World from Java");
+          System.out.println("how are you");   
       }
 }
